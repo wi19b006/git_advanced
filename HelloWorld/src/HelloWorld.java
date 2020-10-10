9848657916401316e541e67e409830ab579cc10b
wi19b006 <72462097+wi19b006@users.noreply.github.com>
2020-10-10 13:21:57 +0000

    Changed Argument-Output Text

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		if(args!=null && args.length>1) {
-			System.out.println("Hello Arguments");
+			System.out.println("Here are your arguments: ");
 			for (String arg : args) {
 				System.out.println(arg);
 			}
