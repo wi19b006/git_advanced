95c682fa855d7b7270311664d687a69828aabbb3
Dominik Meisel <wi19b006@technikum-wien.at>
2020-10-10 13:22:28 +0000

    Modified console outpuut

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		if(args!=null && args.length>1) {
-			System.out.println("Hello Arguments");
+			System.out.println("Printing all arguments:");
 			for (String arg : args) {
 				System.out.println(arg);
 			}
